d15fa6ad8bc50a7a1295afcf97e5c92cd0a0c39d
gmoodyfootball <gmoodyfootball@gmail.com>
2018-01-25 09:37:04 -0700

    Added Classes

---
+++ MegaDesk-3-GregMoody/DeskQuote.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_3_GregMoody
+{
+    class DeskQuote
+    {
+        public Desk desk { get; set; }
+        public int rushDays { get; set; }
+        public string custName { get; set; }
+        public DateTime quoteDate { get; set; }
+        public float totalCost { get; set; }
+
+        //Non-Default Constructor
+        DeskQuote(Desk desk, int rushDays, string custName, DateTime quoteDate)
+        {
+            this.desk = desk;
+            this.rushDays = rushDays;
+            this.custName = custName;
+            this.quoteDate = quoteDate;
+            totalCost = 0;
+        }
+
+        //Default Constructor
+        DeskQuote()
+        {
+            desk = new Desk();
+            rushDays = 0;
+            custName = "NoNameJenkins";
+            quoteDate = new DateTime();
+            totalCost = 0;
+        }
+    }
+}
